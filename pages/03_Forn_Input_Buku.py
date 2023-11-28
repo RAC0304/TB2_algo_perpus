f6d01cecef45a8a64a2596b2fa6cb6b7cce265dd
Rendy Ariesta Chandra <rendyariesta0@gmail.com>
2023-11-28 23:53:20 +0700

    update

--- pages/03_Forn_Input_Buku.py
+++ pages/03_Forn_Input_Buku.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pymysql
 from PIL import Image
 
-st.set_page_config(page_title="Library Book Input Form", page_icon="📚", layout="centered")
+st.set_page_config(page_title="Library Book Input Form",
+                   page_icon="📚", layout="centered")
 
 st.title("Library Book Input Form")
 
@@ -16,6 +17,8 @@ db = pymysql.connect(
 cursor = db.cursor()
 
 # Function to create data in the database
+
+
 def create_data(judul, pengarang, image, penerbit, tahun_terbit, kategori, stok):
     # Save the uploaded image to the ./assets/images/ directory
     if image is not None:
@@ -23,12 +26,16 @@ def create_data(judul, pengarang, image, penerbit, tahun_terbit, kategori, stok)
         image_name = image.name
         with open(f"./assets/images/{image_name}", "wb") as img_file:
             img_file.write(image_content)
+    else:
+        image_name = 'Belum ada gambar'
 
     # Insert the data into the MySQL database
     sql = "INSERT INTO buku (judul, pengarang, image, penerbit, tahun_terbit, kategori, stok) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    val = (judul, pengarang, image_name, penerbit, tahun_terbit, kategori, stok)
+    val = (judul, pengarang, image_name,
+           penerbit, tahun_terbit, kategori, stok)
     cursor.execute(sql, val)
     db.commit()
+
 
 # Create a form for book information
 form = st.form(key='book_input_form')
@@ -36,7 +43,8 @@ form = st.form(key='book_input_form')
 # Add input fields to the form
 judul = form.text_input(label='Judul Buku', max_chars=255)
 pengarang = form.text_input(label='Pengarang', max_chars=255)
-image = form.file_uploader("Upload Book Cover Image", type=["jpg", "jpeg", "png"])
+image = form.file_uploader("Upload Book Cover Image",
+                           type=["jpg", "jpeg", "png"])
 penerbit = form.text_input(label='Penerbit', max_chars=255)
 tahun_terbit = form.date_input(label='Tahun Terbit')
 kategori = form.selectbox(
@@ -50,6 +58,7 @@ submitted = form.form_submit_button(label='Tambah')
 
 # Process form submission
 if submitted:
-    create_data(judul, pengarang, image, penerbit, tahun_terbit, kategori, stok)
+    create_data(judul, pengarang, image, penerbit,
+                tahun_terbit, kategori, stok)
     st.snow()
     st.success("Data telah ditambahkan!")
